3ced7c531fd27b26612c6841db9609df55380ee3
jude <113414494+jude12121@users.noreply.github.com>
2022-09-14 09:54:43 +0530

    Update and rename Apha & Beta pruning algorithm.py to Alpha & Beta pruning algorithm.py

--- Alpha & Beta pruning algorithm.py
+++ Alpha & Beta pruning algorithm.py
@@ -1,27 +1,27 @@
-def minimax(depth, player, alpha, beta)
-  if gameover or depth == 0
+def minimax(depth, player, alpha, beta):
+  if gameover or depth == 0:
     return calculated_score
   end
   children = all legal moves for player
-  if player is AI (maximizing player)
+  if player is AI (maximizing player):
     for each child
       score = minimax(depth - 1, opponent, alpha, beta)
       if (score > alpha)
         alpha = score
       end
-      if alpha >= beta
+      if alpha >= beta:
         break
       end
       return alpha
     end
-  else #player is minimizing player
+  else: #player is minimizing player
     best_score = +infinity
-    for each child
+    for each child:
       score = minimax(depth - 1, player, alpha, beta)
-      if (score < beta)
+      if (score < beta):
         beta = score
       end
-      if alpha >= beta
+      if alpha >= beta:
         break
       end
       return beta
